7e38e7de52bcd77e4245c7637dd95eb8c505d6ce
jimboweb <jimboweb@users.noreply.github.com>
2018-03-02 23:53:57 -0500

    get rid of edgeCount map

--- src/main/java/PrintCircuit.java
+++ src/main/java/PrintCircuit.java
@@ -8,16 +8,8 @@ class PrintCircuit{
      * @param edges list of adjacent vertices for current edges
      * @return circuit in deque list
      */
-    Deque<Integer> makeEulerianCircuit(Deque<Integer>[] edges)
+    Deque<Integer> makeEulerianCircuit(Deque<Integer>[] edges, int numberOfNodes)
     {
-        // edgeCount = number of unused edges from vertex
-        Map<Integer,Integer> edgeCount = new HashMap<>();
-
-        //populate edge count
-        for (int i=0; i<edges.length; i++)
-        {
-            edgeCount.put(i, edges[i].size());
-        }
 
         // return empty list for empty graph
         if (edges.length==0)
@@ -27,7 +19,7 @@ class PrintCircuit{
         Deque<Integer> currentPath = new ArrayDeque<>();
 
         // queue for the final circuit
-        Deque<Integer> circuit = new LinkedList<>();
+        Deque<Integer> circuit = new ArrayDeque<>();
 
         // start from any vertex
         currentPath.push(0);
@@ -35,12 +27,11 @@ class PrintCircuit{
 
         while (!currentPath.isEmpty())
         {
-            if (edgeCount.get(currentVertexNumber) > 0)
+            if (edges[currentVertexNumber].size() > 0)
             {
                 currentPath.push(currentVertexNumber);
                 Deque<Integer> currentVertex = edges[currentVertexNumber];
                 int nextVertexNumber = currentVertex.pop();
-                edgeCount.put(currentVertexNumber,edgeCount.get(currentVertexNumber)-1);
                 currentVertexNumber = nextVertexNumber;
             }
 
@@ -68,31 +59,30 @@ class PrintCircuit{
     private void inputAndPrintCircuit(){
         Scanner scanner = new Scanner(System.in);
         List<List<Integer>> inputs;
-        List<Integer> in = new ArrayList<>();
-        in.add(scanner.nextInt());
-        in.add(scanner.nextInt());
-        if (in.get(0) == 0 || in.get(1) == 0) {
-            System.out.println("0");
-            System.exit(0);
+        int[] in = new int[2];
+        in[0] = scanner.nextInt();
+        in[1] = scanner.nextInt();
+        Deque<Integer>[] edges = new Deque[in[0]];
+        for(int i=0;i<in[0];i++)
+        {
+            edges[i] = new ArrayDeque<>();
         }
-        //set the capacity of the array in advance to save a few microseconds.
-        inputs = new ArrayList<>(in.get(1)+1);
-        inputs.add(in);
-        for (int i = 0; i < inputs.get(0).get(1); i++) {
-            in = new ArrayList<>();
-            in.add(scanner.nextInt());
-            in.add(scanner.nextInt());
-            inputs.add(in);
+        int[][] evenChecker = new int[in[0]][2];
+        for (int i = 0; i < in[1]; i++) {
+            int from = scanner.nextInt()-1;
+            int to = scanner.nextInt()-1;
+            evenChecker[from][0]++;
+            evenChecker[to][1]++;
+            edges[from].push(to);
 
         }
-        Deque<Integer>[] graph = buildGraph(inputs);
-        if(graph.length==0){
+        if(!isGraphEven(evenChecker)){
             System.out.println("0");
             System.exit(0);
         } else {
             System.out.println("1");
         }
-        Deque<Integer> circuit = makeEulerianCircuit(graph);
+        Deque<Integer> circuit = makeEulerianCircuit(edges, in[0]);
         while(circuit.size()>1){
             int nextNode = circuit.pollLast()+1;
             System.out.print(nextNode + " ");
@@ -100,38 +90,6 @@ class PrintCircuit{
         System.out.println();
     }
 
-    /**
-     * create an array of Deques to represent the edges of the graph
-     * @param inputs list of integer pair lists from input. first list is the number of nodes and edges.
-     * @return arrayList of Dequeues to represent the edges in the graph. empty array means graph isn't even and has no Eulerian circuit.
-     */
-    private Deque<Integer>[] buildGraph(List<List<Integer>> inputs){
-        int numberOfNodes = inputs.get(0).get(0);
-        int numberOfEdges = inputs.get(0).get(1);
-        Deque<Integer>[] edges = new Deque[numberOfEdges];
-        for(int i=0;i<edges.length;i++){
-            edges[i] = new ArrayDeque<>();
-        }
-        //evenChecker is array for each node where
-        // [0] = inputs and [1] = outputs.
-
-        int[][] evenChecker = new int[numberOfNodes][2];
-        for(int i=1;i<inputs.size();i++){
-            List<Integer> input = inputs.get(i);
-            int from = input.get(0)-1;
-            int to = input.get(1)-1;
-            evenChecker[from][0]++;
-            evenChecker[to][1]++;
-            edges[from].push(to);
-        }
-
-        //checks to make sure inputs=outputs for each edge.
-        if(!isGraphEven(evenChecker)){
-            return new Deque[0];
-        }
-
-        return edges;
-    }
 
     private boolean isGraphEven(int[][] evenChecker){
         for(int[] evenCheck:evenChecker){
